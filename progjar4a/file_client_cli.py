4ec46e7b616d0af8aeba1cae3429640e8b926e93
adeliarahmaa <162128135+adeliarahmaa@users.noreply.github.com>
2025-05-25 04:13:08 +0700

    Update file_client_cli.py

--- progjar4a/file_client_cli.py
+++ progjar4a/file_client_cli.py
@@ -2,109 +2,90 @@ import socket
 import json
 import base64
 import logging
-from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor
+from concurrent.futures import ThreadPoolExecutor
 
-# logging
-import csv
-import os 
-import time
-import threading
-
-server_address=('0.0.0.0',7777)
+server_address = ('172.16.16.101', 9999)
+logging.basicConfig(level=logging.WARNING)
 
 def send_command(command_str=""):
     global server_address
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.connect(server_address)
-    logging.warning(f"connecting to {server_address}")
-
-    # Command delimiter
-    if not command_str.endswith("\r\n\r\n"):
-        command_str += "\r\n\r\n"
+    logging.warning(f"Connecting to {server_address}")
     try:
-        logging.warning(f"sending message ")
+        logging.warning(f"Sending message")
         sock.sendall(command_str.encode())
-        # Look for the response, waiting until socket is done (no more data)
-        data_received="" #empty string
+
+        data_received = ""
         while True:
-            #socket does not receive all data at once, data comes in part, need to be concatenated at the end of process
             data = sock.recv(16)
             if data:
-                #data is not empty, concat with previous content
                 data_received += data.decode()
                 if "\r\n\r\n" in data_received:
                     break
             else:
-                # no more data, stop the process by break
                 break
-        # at this point, data_received (string) will contain all data coming from the socket
-        # to be able to use the data_received as a dict, need to load it using json.loads()
+
         hasil = json.loads(data_received)
-        logging.warning("data received from server:")
+        logging.warning("Data received from server:")
         return hasil
     except:
-        logging.warning("error during data receiving")
+        logging.warning("Error during data receiving")
         return False
-
 
 def remote_list():
-    command_str=f"LIST"
+    command_str = "LIST"
     hasil = send_command(command_str)
-    if (hasil['status']=='OK'):
-        print("daftar file : ")
+    if hasil and hasil['status'] == 'OK':
+        print("== Daftar file ==")
         for nmfile in hasil['data']:
             print(f"- {nmfile}")
-        return True
     else:
         print("Gagal")
-        return False
 
 def remote_get(filename=""):
-    command_str=f"GET {filename}"
+    command_str = f"GET {filename}"
     hasil = send_command(command_str)
-    if (hasil['status']=='OK'):
-        #proses file dalam bentuk base64 ke bentuk bytes
-        namafile= hasil['data_namafile']
+    if hasil and hasil['status'] == 'OK':
+        namafile = hasil['data_namafile']
         isifile = base64.b64decode(hasil['data_file'])
-        fp = open(namafile,'wb+')
-        #fp.write(isifile)
-        fp.close()
-        return True
+        with open(namafile, 'wb') as fp:
+            fp.write(isifile)
     else:
         print("Gagal")
-        return False
 
-def remote_upload(filepath=""):
-    filecontent = f"{convert_file(filepath)}"
-    command_str=f"UPLOAD {filepath} {filecontent}"
-    hasil = send_command(command_str)
-    if(hasil['status']=='OK'):
-        print(f"Berhasil Mengupload {filepath}")
-        return True
-    else:
-        print(f"Gagal Mengupload {filepath}")
-        return False
-
-def convert_file(filepath):
+def remote_upload(filename=""):
     try:
-        with open(filepath, 'rb') as f:
-            encoded = base64.b64encode(f.read())
-            return encoded.decode("utf-8")
-    except Exception as e:
-        print(f"Gagal mengonversi file: {e}")
-        return False
+        with open(filename, 'rb') as file:
+            isifile = base64.b64encode(file.read()).decode()
+        command_str = f"upload {filename} {isifile}"
+        hasil = send_command(command_str)
+        if hasil and hasil['status'] == 'OK':
+            print(f" File '{filename}' berhasil diupload")
+        else:
+            print(f" Gagal upload file: {filename}")
+    except FileNotFoundError:
+        print(f" File '{filename}' tidak ditemukan")
+
+def remote_download(filename=""):
+    command_str = f"GET {filename}"
+    hasil = send_command(command_str)
+    if hasil and hasil['status'] == 'OK':
+        namafile = hasil['data_namafile']
+        isifile = base64.b64decode(hasil['data_file'])
+        with open(namafile, 'wb') as fp:
+            fp.write(isifile)
+        print(f" File '{filename}' berhasil diunduh")
+    else:
+        print(f" Gagal mengunduh file: {filename}")
 
 
-if __name__=='__main__':
-    server_address=('0.0.0.0',6666)
-    
-    # remote_list()
-    # remote_get('donalbebek.jpg')
-    # remote_upload('PROTOKOL.txt')
-    # print(f"{convert_file('PROTOKOL.txt')}")
-    
+if __name__ == '__main__':
+    server_address=(('172.16.16.101', 9999))
+    file_target = 'donalbebek.jpg'
 
-    with ThreadPoolExecutor(max_workers=1) as executor:
+    with ThreadPoolExecutor(max_workers=4) as executor:
         executor.submit(remote_list)
-        executor.submit(remote_get, 'donalbebek.jpg')
-        executor.submit(remote_upload, 'PROTOKOL.txt')
+        executor.submit(remote_get, file_target)
+        executor.submit(remote_upload, file_target)
+        executor.submit(remote_download, file_target)
